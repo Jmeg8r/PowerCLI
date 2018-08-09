313ff4a00a7930dd46578b2aedb76fae535918a7
James Cruce <jmegtr@gmail.com>
2018-08-09 09:44:45 -0400

    Formatted comments

--- Set-Crypto.ps1
+++ Set-Crypto.ps1
@@ -2,7 +2,11 @@
 #Date: 6/27/2018
 # Version 1.0
 # Script assumes you have vCenter admin rights and are currently connected to the vCenter with all the vms in the vms.txt file
-# The script will load the list of vms to configure cipher settings and then copy over the needed IISCrypto CLI program ( https://www.nartac.com/Products/IISCrypto ) as well as the needed template files and the bat files to execute the IISCrypto cli command which will apply the correct template on the operating system version. It will then clean up the unneeded files and IISCrypto folder on the remote vm. It will loop through the entire list until completed.
+# The script will load the list of vms to configure cipher settings and then copy over the needed IISCrypto CLI program 
+# ( https://www.nartac.com/Products/IISCrypto ) as well as the needed template files and the bat files to execute the 
+# IISCrypto cli command which will apply the correct template on the operating system version.
+# It will then clean up the unneeded files and IISCrypto folder on the remote vm.
+# It will loop through the entire list until completed.
 
 
 
