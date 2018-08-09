177f6ed080f67d8511d715f619dc59f93d3b83cb
James Cruce <jmegtr@gmail.com>
2018-08-09 09:52:08 -0400

    IIS Crypto Bat Files
    Bat Files with preconfigured IIS Crypto profiles configured for Windows Server 2012 and Windows Server 2016 respectively

---
+++ ConfigCrypto12.bat
@@ -0,0 +1,2 @@
+cd c:\IISCrypto\
+c:\IISCrypto\IISCryptoCli.exe /template c:\IISCrypto\BP2K12R2.ictpl
---
+++ ConfigCrypto16.bat
@@ -0,0 +1,2 @@
+cd c:\IISCrypto\
+c:\IISCrypto\IISCryptoCli.exe /template c:\IISCrypto\BP2K16.ictpl
